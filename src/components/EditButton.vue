2efe6b02a498270dea3f6315476caf7b9a4be9aa
FUGA0618 <aax.chiri@gmail.com>
2021-07-03 11:11:19 +0900

    EditButtonコンポーネントを実装

---
+++ src/components/EditButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <a @click="toggleEditFlg">
+    <i class="bi bi-pencil-fill"></i>編集
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'EditButton',
+  methods: {
+    toggleEditFlg () {
+      this.$emit('toggleEditFlg')
+    }
+  }
+}
+</script>
+
+<style>
+a:hover {
+  cursor: pointer;
+}
+</style>
