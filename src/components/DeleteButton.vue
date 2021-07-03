f403056a3d9ed9cb6834ed359d94fc785bbe493c
FUGA0618 <aax.chiri@gmail.com>
2021-07-03 11:12:08 +0900

    DeleteButtonコンポーネントを実装

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <a @click="deleteTodo">
+    <i class="bi bi-trash-fill"></i>削除
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'DeleteButton',
+  methods: {
+    deleteTodo () {
+      this.$emit('deleteTodo')
+    }
+  }
+}
+</script>
+
+<style>
+a:hover {
+  cursor: pointer;
+}
+</style>
