379fd617c89b28ba05637c6d63ff4449e66f73ae
FUGA0618 <aax.chiri@gmail.com>
2021-07-03 11:08:22 +0900

    TodoListコンポーネントを実装

---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="list-group">
+    <ListItem v-for="(content, index) in todoItems"
+              :index="index"
+              :content="content"
+              :key="index"
+              @delete-todo="deleteTodo"
+              @update-todo="updateTodo" />
+  </ul>
+</template>
+
+<script>
+import ListItem from "./ListItem.vue"
+
+export default {
+  name: 'TodoList',
+  props: ['todoItems'],
+  components: {
+    ListItem
+  },
+  methods: {
+    updateTodo (...newTodo) {
+      this.$emit('updateTodo', newTodo)
+    },
+    deleteTodo (index) {
+      this.$emit('deleteTodo', index)
+    }
+  }
+}
+</script>
